f152cdeddada27ed0f6547fa705bb431649583af
teripanda <teri.london@gmail.com>
2021-06-23 04:34:11 -0700

    refactor to jquery

--- script.js
+++ script.js
@@ -1,41 +1,29 @@
-async function handleWeatherRequest(event) {
-	event.preventDefault();
+async function handleWeatherRequest(e) {
+	e.preventDefault();
 
-	const city = document.querySelector('input[name="city"]').value;
+	const city = $('input[name="city"]').val();
 
 	const apiKey = "870b1b10b58578f725b70b13aff6c357";
 
-	const urlString =
-		"https://api.openweathermap.org/data/2.5/weather?q=" +
-		city +
-		"&APPID=" +
-		apiKey;
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`;
 
-	const response = await fetch(urlString);
+	const response = await fetch(url);
 	const data = await response.json();
 
-	// The temp is in Kelvin: not nice for humans!
+	// Convert Kelvin to Fahrenheit
 	const readableTemp = ((data.main.temp - 273.15) * 9) / 5 + 32;
-
 	const tempFeelsLike = ((data.main.feels_like - 273.15) * 9) / 5 + 32;
-
 	const humidity = data.main.humidity;
-
 	const clouds = data.clouds.all;
 
-	document.querySelector("#weather-result").innerText =
-		"The weather in " + city + " is currently " + readableTemp;
-
-	document.querySelector("#weather-feeling").innerText =
-		"But the weather in " + city + " feels like " + tempFeelsLike;
-
-	document.querySelector("#humidity").innerText =
-		"Also, the humidity in " + city + " is " + humidity + "%!";
-
-	document.querySelector("#clouds").innerText =
-		"And the cloud percentage is " + clouds + "!";
+	$("#weather-result").text(
+		`The weather in ${city} is currently ${readableTemp}`
+	);
+	$("#weather-feeling").text(
+		`But the weather in ${city} feels like ${tempFeelsLike}`
+	);
+	$("#humidity").text(`Also, the humidity in ${city} is ${humidity}%!`);
+	$("#clouds").text(`And the cloud percentage is ${clouds}!`);
 }
 
-document
-	.querySelector("#weather-search")
-	.addEventListener("submit", handleWeatherRequest);
+$("#weather-search").on("submit", handleWeatherRequest);
